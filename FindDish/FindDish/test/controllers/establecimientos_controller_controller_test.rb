63a5a2420fb84f6526c4fb2cdf8f42cd58ebc78a
ladyrod <l_rodrigues@correo.unimet.edu.ve>
2016-11-02 12:46:39 -0400

    Add files via upload

---
+++ FindDish/FindDish/test/controllers/establecimientos_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EstablecimientosControllerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ FindDish/FindDish/test/controllers/establecimientos_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EstablecimientosControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
