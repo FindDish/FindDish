389c4e0ccade40aa8ac13fbe90d6572df877e50c
ladyrod <l_rodrigues@correo.unimet.edu.ve>
2016-11-30 21:02:58 -0400

    Update routes.rb

--- FindDish/config/routes.rb
+++ FindDish/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root to: 'establecimientos'
+root to: 'vista#index'
 resources :usuarios
 root 'usuarios#index'
 
@@ -11,8 +11,46 @@ root 'platillos#index'
 
 resources :reservas
 root 'reservas#index'
-  
+
+resources :tareas  do
+  member do
+    get 'Ver'
+ 	  get 'Query1'
+    get 'Query2'
+    get 'Query3'
+    get 'Query4'
+  end
+  collection do
+    get 'Ver'
+    get 'Query1'
+    get 'Query2'
+    get 'Query3'
+    get 'Query4'
+  end
+end
+
 resources :tareas
 root 'tareas#index'
+
+resources :alergias
+root 'alergias#index'
+
+resources :telefonos
+root 'telefonos#index'
+
+resources :establecimientos
+root 'establecimientos#index'
+
+resources :ordenes
+root 'ordenes#index'
+
+resources :alergias
+root 'alergias#index'
+
+resources :tareas
+root 'tareas#index'
+
+resources :vista
+root 'vista#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
